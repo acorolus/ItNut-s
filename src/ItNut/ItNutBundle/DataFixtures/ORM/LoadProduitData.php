5fb4ba0dcd5936400aaa373a771dcbe6af2fef0d
acorolus <a.corolus@gmail.com>
2016-11-25 00:43:03 +0100

    last

--- src/ItNut/ItNutBundle/DataFixtures/ORM/LoadProduitData.php
+++ src/ItNut/ItNutBundle/DataFixtures/ORM/LoadProduitData.php
@@ -17,30 +17,35 @@ class LoadProduitData implements FixtureInterface
         $produit1->setShortcode("Mélange-pour-Boisson-Protéinée");
         $produit1->setImage("nomDeFichier");
 
+        $produit2 = new Produit();
         $produit2->setNom("Graines de soja grillées");
         $produit2->setDescription("Ce délicieux encas salé est idéal si vous êtes en déplacement. Chaque sachet contient 9g de protéines et 113 kcal. Les Graines de Soja Grillées constituent une excellente alternative aux autres encas salés tels que les chips ou les biscuits apéritifs.<br/><b>\r\n\r\nAPPORTS ESSENTIELS</b><br/><ul><li>\r\nRiches en protéines (9g par sachet) qui contribuent à augmenter la masse musculaire.</li><li>\r\n113 calories par sachet, soit moins que dans les encas salés classiques.</li><li>\r\nDes graines grillées à sec et légèrement salées – un vrai délice.</li><li>\r\nEn sachet, un format très pratique pour les déplacements.</li></ul><br/><b>\r\nAVANTAGES PRODUIT</b><br/><ul><li>\r\nUn encas équilibré et gourmand.</li><li>\r\nUne alternative aux encas salés tels que les chips, les biscuits apéritifs et les fruits secs salés, ou si vous souhaitez contrôler votre poids.</li><li>\r\nAide à augmenter votre consommation de protéines.</li><li>\r\nIdéal si vous menez une vie très active et si vous avez envie d’un encas équilibré alors que vous êtes en déplacement.</li></ul>");
         $produit2->setPrix(30.99);
         $produit2->setShortcode("herbalife-graines-soja-grillees");
         $produit2->setImage("");
 
+        $produit3 = new Produit();
         $produit3->setNom("Boulette de viande bio");
         $produit3->setDescription("Voici les nouvelles boulettes de viande bio faites par Herbalife.\r\n\r\nProduites dans nos fermes bio, nous vous garantissons une qualité maximale.");
         $produit3->setPrix(90);
         $produit3->setShortcode("Boulette-de-viande-bio");
         $produit3->setImage("");
 
+        $produit4 = new Produit();
         $produit4->setNom("chocolat");
         $produit4->setDescription("chocolat chaud");
         $produit4->setPrix(5);
         $produit4->setShortcode("chocolat");
         $produit4->setImage("");
 
+        $produit5 = new Produit();
         $produit5->setNom("pack");
         $produit5->setDescription("pack petit déjeuner");
         $produit5->setPrix(122.5);
         $produit5->setShortcode("pack");
         $produit5->setImage("7a8cffce06f088f3e11279f235c04a66.png");
 
+        $produit6 = new Produit();
         $produit6->setNom("Aloé Mangue");
         $produit6->setDescription("Concentré  d\'aloé vera.");
         $produit6->setPrix(41.5);
